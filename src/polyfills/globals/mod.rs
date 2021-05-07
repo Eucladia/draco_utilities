3048530f2d62575d9e08e4a451735df0fb42be15
Eucladia <EucladiaLyra@gmail.com>
2021-05-07 12:48:55 -0400

    ref: encapsulate uri functions into its own module

--- src/polyfills/globals/mod.rs
+++ src/polyfills/globals/mod.rs
@@ -1,18 +1,22 @@
-/// Decoding URIs.
-pub mod decode_uri;
-/// Encoding URIs.
-pub mod encode_uri;
+mod decode_uri;
+mod encode_uri;
+
 /// Global utilities.
 pub mod utils;
 
-pub use decode_uri::*;
-pub use encode_uri::*;
+pub use uri::*;
 
-/// An error when encoding or decoding a URI.
-#[derive(Debug, Copy, Clone, Hash, PartialEq, Eq)]
-pub enum UriError {
-  /// The provided URI was malformed.
-  InvalidUri,
-  /// An unexpected character was encountered.
-  InvalidUtf8Character,
+/// URI related functions
+pub mod uri {
+  pub use super::decode_uri::*;
+  pub use super::encode_uri::*;
+
+  /// An error when encoding or decoding a URI.
+  #[derive(Debug, Copy, Clone, Hash, PartialEq, Eq)]
+  pub enum UriError {
+    /// The provided URI was malformed.
+    InvalidUri,
+    /// An unexpected character was encountered.
+    InvalidUtf8Character,
+  }
 }
