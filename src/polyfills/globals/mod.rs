964b2d3b48b721970f67928abacd52d1d901c2e0
Eucladia <EucladiaLyra@gmail.com>
2021-05-17 16:24:36 -0400

    ref: remove uri module encapsulation

--- src/polyfills/globals/mod.rs
+++ src/polyfills/globals/mod.rs
@@ -6,21 +6,16 @@ mod unescape;
 /// Global utilities.
 pub mod utils;
 
+pub use decode_uri::*;
+pub use encode_uri::*;
 pub use escape::escape;
 pub use unescape::unescape;
-pub use uri::*;
 
-/// URI related functions
-pub mod uri {
-  pub use super::decode_uri::*;
-  pub use super::encode_uri::*;
-
-  /// An error when encoding or decoding a URI.
-  #[derive(Debug, Copy, Clone, Hash, PartialEq, Eq)]
-  pub enum UriError {
-    /// The provided URI was malformed.
-    InvalidUri,
-    /// An unexpected character was encountered.
-    InvalidUtf8Character,
-  }
+/// An error when encoding or decoding a URI.
+#[derive(Debug, Copy, Clone, Hash, PartialEq, Eq)]
+pub enum UriError {
+  /// The provided URI was malformed.
+  InvalidUri,
+  /// An unexpected character was encountered.
+  InvalidUtf8Character,
 }
